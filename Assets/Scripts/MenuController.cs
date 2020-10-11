f6b67d9a46e8dc806d46c9a863366fb7303f0c22
tiagorc <eulercarvalho.se@gmail.com>
2020-10-11 09:37:23 -0300

    Showing tip when hover mouse in some button

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Text tipText;
+    public Text tipTextShadow;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void openScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit()
+#endif
+    }
+
+    public void showTip(string tip)
+    {
+        tipText.text = tip;
+        tipTextShadow.text = tip;
+    }
+}
